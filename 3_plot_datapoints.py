0e21b2c6937fa26a2f15cdca2531fa2677a55fe7
mirandadam <mirandadam@users.noreply.github.com>
2018-11-13 00:42:05 -0200

    added pareto frontier calculation

--- 3_plot_datapoints.py
+++ 3_plot_datapoints.py
@@ -40,6 +40,7 @@ for n in profile_names.values():
     fig.subplots_adjust(bottom=0.15)
     ax.set_yscale('log')
     ax.set_ylabel('Transmission speed (cps)')
+    point_set = []
     for p in simulation_profiles:
         if n != profile_names[p[1]]:
             continue
@@ -50,11 +51,37 @@ for n in profile_names.values():
         speed = [i['speed (char/s)'] for i in mode_info if i['mode'] == p[0]][0]
         #speed = np.log10(speed)
         if cut_point >= 60:
-            print('Mode unable to decode with required accuracy:')
-            print('', (p[0], n, speed, cut_point))
+            #print('Mode unable to decode with required accuracy:')
+            #print('', (p[0], n, speed, cut_point))
             continue
-
+        #if 'THOR11' == p[0]:
+        #    print('', (p[0], n, speed, cut_point))
+        point_set.append((speed, cut_point, p[0]))
         ax.scatter(cut_point, speed)
         ax.annotate(p[0], xy=(cut_point, speed), fontsize=8)
+    point_set.sort(reverse=True)
+    pareto_set = np.zeros(len(point_set), dtype='bool')
+    pareto_set[0] = True
+    sensitivity = point_set[0][1]  # best sensitivity seen so far
+    for i in range(len(point_set)-1):
+        # we already know that the speed is decrescent, so any point
+        # down the line has to improve on sensitivity to be included
+        # in the pareto frontier
+        if point_set[i+1][1] < sensitivity:
+            # if the sensitivity of the next point is better than all previous ones
+            sensitivity = point_set[i+1][1]
+            pareto_set[i+1] = True
+        if (point_set[i+1][0] == point_set[i][0] and
+                point_set[i+1][1] == point_set[i][1]):
+                # we also add equal points to the pareto set
+            pareto_set[i+1] = pareto_set[i]
+    print('')
+    print(n)
+    print('')
+    print("Modes in the pareto frontier:")
+    print('| Mode | Speed (cps) | S/N 1% |')
+    print('| ---- | ----------- | ------ |')
+    for aux in [point_set[i] for i in np.where(pareto_set)[0]]:
+        print('|', aux[2], '|', round(aux[0], 1), '|', round(aux[1], 1), '|')
     fig.savefig(dn_results_folder+n+'.png')
 plt.show()
