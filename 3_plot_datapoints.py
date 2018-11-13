f3cf2d65bc9cde2b4da77ab101dcd6a8f9313afa
mirandadam <mirandadam@users.noreply.github.com>
2018-11-12 23:17:06 -0200

    Added labels, grouped by profile, graphics are now saved.

--- 3_plot_datapoints.py
+++ 3_plot_datapoints.py
@@ -25,24 +25,36 @@ for d in datapoints:
     e = [[parameters['awgn s/n'], d['error rate']]]
     simulation_profiles[p] = simulation_profiles.get(p, []) + e
 
-for p in simulation_profiles:
-    x, y = np.array(sorted(simulation_profiles[p])).transpose()
-    y = np.minimum(1., y)
-    cut_point = (t.suggest_samples(x, y, 1/100, 0.)+[40])[0]
-    n = profile_names[p[1]]
-    #if n != 'Mid_Latitude_Disturbed':
-    if n != 'Mid_Latitude_Disturbed_NVIS':
-    #if n != 'Frequency_Shifter':
-    #if n != 'Direct_Path':
-        continue
-    #if 'psk' not in p[0].lower():
-    #    continue
-    speed = [i['speed (char/s)'] for i in mode_info if i['mode'] == p[0]][0]
-    speed = np.log10(speed)
-    print((p[0], n, speed, cut_point))
-    plt.scatter(cut_point, speed)
-    plt.annotate(p[0],
-                 xy=(cut_point, speed))
+# plotting the 1% point for all the profiles:
+for n in profile_names.values():
+    fig = plt.figure()
+    fig.set_size_inches(10, 6)
+    ax = plt.axes()
+    plt.title('Linsim profile: '+n.replace('_', ' '))
+    ax.set_xscale('linear')
+    ax.set_xlabel(
+        'Signal to noise ratio in dB where error rate is 1%\n'
+        'Modes not displayed where unable to decode with 1% or better error rate.\n'
+        'Test message has 712 characters in pt-BR language (t2_cancao_do_exilio.txt).',
+    )
+    fig.subplots_adjust(bottom=0.15)
+    ax.set_yscale('log')
+    ax.set_ylabel('Transmission speed (cps)')
+    for p in simulation_profiles:
+        if n != profile_names[p[1]]:
+            continue
+        x, y = np.array(sorted(simulation_profiles[p])).transpose()
+        y = np.minimum(1., y)
+        cut_point = (t.suggest_samples(x, y, 1/100, 0.)+[60])[0]
 
-#plt.plot(x, y)
+        speed = [i['speed (char/s)'] for i in mode_info if i['mode'] == p[0]][0]
+        #speed = np.log10(speed)
+        if cut_point >= 60:
+            print('Mode unable to decode with required accuracy:')
+            print('', (p[0], n, speed, cut_point))
+            continue
+
+        ax.scatter(cut_point, speed)
+        ax.annotate(p[0], xy=(cut_point, speed), fontsize=8)
+    fig.savefig(dn_results_folder+n+'.png')
 plt.show()
